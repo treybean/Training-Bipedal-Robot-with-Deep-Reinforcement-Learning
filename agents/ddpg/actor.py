f7067054c42f02a6bcaa2efa0c220c64acc9410d
Trey Bean <trey.bean@icloud.com>
2019-04-23 18:09:28 -0600

    feat: Initial implementation of DDPG agent

---
+++ agents/ddpg/actor.py
@@ -0,0 +1,89 @@
+from keras import layers, models, optimizers, regularizers
+from keras import backend as K
+
+
+class Actor:
+    """Actor (Policy) Model."""
+
+    def __init__(self, env):
+        """Initialize parameters and build model."""
+
+        self.state_size = env.observation_space.shape[0]
+        self.action_size = env.action_space.shape[0]
+        self.action_low = env.action_space.low
+        self.action_high = env.action_space.high
+        self.action_range = self.action_high - self.action_low
+
+        # Initialize any other variables here
+
+        self.build_model()
+
+    def build_model(self):
+        """Build an actor (policy) network that maps states -> actions."""
+        # Define input layer (states)
+        states = layers.Input(shape=(self.state_size,), name="states")
+
+        # Add hidden layers
+        net = layers.Dense(units=32)(states)
+        net = layers.BatchNormalization()(net)
+        net = layers.Activation("relu")(net)
+        net = layers.Dense(units=64)(net)
+        net = layers.BatchNormalization()(net)
+        net = layers.Activation("relu")(net)
+        net = layers.Dense(units=32)(net)
+        net = layers.BatchNormalization()(net)
+        net = layers.Activation("relu")(net)
+
+        # net = layers.Dense(units=400, kernel_regularizer=layers.regularizers.l2(1e-2))(
+        #     states
+        # )
+        # net = layers.BatchNormalization()(net)
+        # net = layers.Activation("relu")(net)
+        # net = layers.Dense(units=300, kernel_regularizer=layers.regularizers.l2(1e-2))(
+        #     net
+        # )
+        # net = layers.BatchNormalization()(net)
+        # net = layers.Activation("relu")(net)
+
+        # # Add final output layer with sigmoid activation
+        # raw_actions = layers.Dense(
+        #     units=self.action_size,
+        #     activation="sigmoid",
+        #     name="raw_actions",
+        #     kernel_initializer=layers.initializers.RandomUniform(
+        #         minval=-0.003, maxval=0.003
+        #     ),
+        # )(net)
+
+        # Try different layer sizes, activations, add batch normalization, regularizers, etc.
+
+        # Add final output layer with sigmoid activation
+        raw_actions = layers.Dense(
+            units=self.action_size, activation="sigmoid", name="raw_actions"
+        )(net)
+
+        # Scale [0, 1] output for each action dimension to proper range
+        actions = layers.Lambda(
+            lambda x: (x * self.action_range) + self.action_low, name="actions"
+        )(raw_actions)
+
+        # Create Keras model
+        self.model = models.Model(inputs=states, outputs=actions)
+
+        # Define loss function using action value (Q value) gradients
+        action_gradients = layers.Input(shape=(self.action_size,))
+        loss = K.mean(-action_gradients * actions)
+
+        # Incorporate any additional losses here (e.g. from regularizers)
+
+        # Define optimizer and training function
+        optimizer = optimizers.Adam(lr=1e-4)
+        updates_op = optimizer.get_updates(
+            params=self.model.trainable_weights, loss=loss
+        )
+        self.train_fn = K.function(
+            inputs=[self.model.input, action_gradients, K.learning_phase()],
+            outputs=[],
+            updates=updates_op,
+        )
+
--- main.py
+++ main.py
@@ -1,21 +1,41 @@
 import gym
-from gym.wrappers.monitoring.video_recorder import VideoRecorder
+
+# from gym.wrappers.monitoring.video_recorder import VideoRecorder
 import sys
+from agents.ddpg.agent import DDPG
 
 env = gym.make("BipedalWalker-v2")
 # video = VideoRecorder(env, base_path="./video")
 
-for i_episode in range(50):
-    observation = env.reset()
+agent = DDPG(env)
+episode_count = 3000
+
+for i in range(episode_count):
+    episode_reward = 0
+    # TODO: Explore shifting this back to env.reset()
+    observation = agent.reset_episode()
+
     for t in range(1600):
+        # Render
         # env.render()
         # video.capture_frame()
-        # print(observation)
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        # print(reward)
+
+        # Determine next action and reward based on state
+        action = agent.act(observation)
+        next_observation, reward, done, info = env.step(action)
+
+        # Capture reward
+        episode_reward += reward
+
+        # Have agent take action
+        agent.step(action, reward, next_observation, done)
+        # TODO: Explore if we can just capture observation when env.steps on line 23
+        observation = next_observation
+
         if done:
-            print(f"Episode {i_episode + 1} finished after {t+1} timesteps")
+            print(
+                f"Episode {i + 1} finished after {t + 1} timesteps. Reward: {episode_reward}. Final hull x: {env.hull.position.x}"
+            )
             break
 
     sys.stdout.flush()
